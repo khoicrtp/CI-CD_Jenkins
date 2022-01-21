4dfb5e9096fd9f8c71b0507cba209b38f1b5419c
19127181 <bachminhkhoicr@gmail.com>
2022-01-22 04:27:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Hello World AGAIN AGAIN AGAIN Helu !\n'
-
-@app.route('/hello_world/')
-def hello_world():
-    return 'Very Hello World !\n'
-
-@app.route('/<username>') # dynamic route
-def hello_user(username):
-    return 'Very Welcome back, %s!\n' % username
+    return 'Hello World !\n'
 
 if __name__ == '__main__':
-    app.run()#host='0.0.0.0')     # open for everyone
+    app.run()#host='0.0.0.0')
